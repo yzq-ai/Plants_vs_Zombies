b2e48e05fb4d3e7c69ad00f9462e336d811d4501
Yang Bu Ji <820943775@qq.com>
2023-07-03 19:25:02 +0800

    完成豌豆子弹
    完成豌豆子弹

--- PVZ/PVZ/main.cpp
+++ PVZ/PVZ/main.cpp
@@ -1,5 +1,5 @@
 
-
+#include <cmath>
 #include <ctime>
 #include <cstdio>
 #include <fstream>
@@ -28,7 +28,10 @@ IMAGE ImgBar;				//工具栏图片
 IMAGE ImgCards[KU_GUA];		//卡牌数组
 IMAGE* ImgPlant[KU_GUA][20];//植物数组
 IMAGE ImgSunShineBall[29];	//阳光球
-IMAGE IMGZombie[22];		//僵尸
+IMAGE ImgZombie[22];		//僵尸
+IMAGE ImgBulletNormal;		//正常子弹
+IMAGE ImgBulletBlast[4];		//爆照子弹
+
 
 
 struct  Plant //植物结构体
@@ -44,6 +47,10 @@ struct  SunShineBall //阳光球结构体
 	int goalY;		//目标位置
 	bool used;      //是否正在使用
 	int timer;		//计时器
+
+	float xOff;		//飞跃的x
+	float yOff;		//飞跃的y
+
 }SunPool[10];	//阳光池
 
 struct  zombie//僵尸结构体
@@ -52,8 +59,19 @@ struct  zombie//僵尸结构体
 	int frameIndex;
 	bool used;
 	int speed;
+	int row;
+	int blood;
 }Zombies[10];
 
+struct  bullet //子弹结构体
+{
+	int x, y;
+	int row;
+	int speed;
+	bool used;
+	bool blast;		//是否爆炸
+	int frameindex;	//序列帧
+}Bullets[30]; 
 
 //文件是否存在
 bool FileExist(const char* name) {
@@ -68,13 +86,22 @@ void GameInit() {
 	memset(map, 0, sizeof map);				//初始化地图数组
 	memset(SunPool, 0, sizeof SunPool);		//初始化阳光池
 	memset(Zombies, 0, sizeof Zombies);		//初始化僵尸数组
+	memset(Bullets, 0, sizeof Bullets);		//初始化子弹数组
+//	memset(ImgBulletBlast,0,sizeof ImgBulletBlast);//
 
 	//加载图片,图片放在内存变量中
-	loadimage(&ImgBg, "res/bg.jpg");
-	loadimage(&ImgBar, "res/bar5.png");
-
-
-
+	loadimage(&ImgBg, "res/bg.jpg");							//背景图片
+	loadimage(&ImgBar, "res/bar5.png");							//卡牌框图片
+	loadimage(&ImgBulletNormal,"res/bullets/bullet_normal.png");//普通子弹图片
+	loadimage(&ImgBulletBlast[3], "res/bullets/bullet_blast.png");	//爆炸子弹图片
+	//初始化爆炸子弹
+	for (int i = 0; i < 3; i++) {
+		float k = (i + 1) * 0.2;
+		loadimage(&ImgBulletBlast[i], "res/bullets/bullet_blast.png",
+			ImgBulletBlast[3].getwidth() * k,
+			ImgBulletBlast[3].getheight() * k, true);	//爆炸子弹图片
+	
+	}
 	//初始化植物卡牌
 	char name[64];
 	for (int i = 0; i < KU_GUA; i++) {
@@ -103,14 +130,16 @@ void GameInit() {
 	//加载僵尸图片
 	for (int i = 0; i < 22; i++) {
 		sprintf_s(name,sizeof name,"res/zb/%d.png",i+1);
-		loadimage(&IMGZombie[i], name);
+		loadimage(&ImgZombie[i], name);
 	}
+
+
 
 	//配置随机数种子
 	srand(time(NULL));
 
 	//创建游戏图形窗口
-	initgraph(W_width, W_height, 0);
+	initgraph(W_width, W_height, 1);
 
 	//设置字体
 	LOGFONT	font;
@@ -122,6 +151,18 @@ void GameInit() {
 	settextstyle(&font);
 	setbkmode(TRANSPARENT);					//字体模式：背景透明
 	setcolor(BLACK);						//字体颜色：黑色
+
+}
+//绘制僵尸
+void DrawZombie(){
+	int ZbCount = sizeof(Zombies) / sizeof(Zombies[0]);
+	for (int i = 0; i < ZbCount; i++) {
+		if (Zombies[i].used) {
+			IMAGE* img = &ImgZombie[Zombies[i].frameIndex];
+			putimagePNG(Zombies[i].x,Zombies[i].y - img->getheight(),img);
+
+		}
+	}
 
 }
 
@@ -160,7 +201,7 @@ void UpdateWindow() {
 
 	int BallMax = sizeof(SunPool) / sizeof(SunPool[0]);//可用阳光的最大数量
 	for (int i = 0; i < BallMax; i++) {
-		if (SunPool[i].used) {
+		if (SunPool[i].used || SunPool[i].xOff) {
 			IMAGE* img = &ImgSunShineBall[SunPool[i].frameIndex];
 			putimagePNG(SunPool[i].x,SunPool[i].y,img);
 		}
@@ -171,6 +212,21 @@ void UpdateWindow() {
 	sprintf_s(ScoreText, sizeof ScoreText, "%d", SunValue);
 	outtextxy(285,67,ScoreText);//输出阳光值
 
+
+	DrawZombie();//绘制僵尸
+	//渲染子弹
+	int BulletMax = sizeof(Bullets) / sizeof(Bullets[0]);
+	for (int i = 0; i < BulletMax; i++) {
+		if (Bullets[i].used) {
+			if (Bullets[i].blast) {
+				IMAGE* img = &ImgBulletBlast[Bullets[i].frameindex];
+				putimagePNG(Bullets[i].x, Bullets[i].y, img);
+			}
+			else {
+				putimagePNG(Bullets[i].x, Bullets[i].y, &ImgBulletNormal);
+			}
+		}
+	}
 	EndBatchDraw();//结束双缓冲
 }
 //收集阳光
@@ -185,10 +241,14 @@ void CollectSunShine(const ExMessage* msg) {
 			if (msg->x > x && msg->x < x + w && 
 				msg->y> y && msg->y < y + h) {
 				SunPool[i].used = false;
-				SunValue += 25;
 				mciSendString("play res/sunshine.mp3", 0, 0, 0);
+				//设置阳光偏移量
+				float goalX = 262;
+				float goalY = 0;
+				float angle = atan((y - goalY) / (x - goalX));
+				SunPool[i].xOff = 4 * cos(angle);
+				SunPool[i].yOff = 4 * sin(angle);
 			}
-
 		}
 	}
 }
@@ -256,6 +316,8 @@ void CreateSunShine() {
 		SunPool[i].y = 60;
 		SunPool[i].goalY = 200 + (rand() % 4) * 90;
 		SunPool[i].timer = 0;
+		SunPool[i].xOff = 0;
+		SunPool[i].yOff = 0;
 	}
 	
 	
@@ -267,7 +329,7 @@ void UpdateSunshine(){
 		if (SunPool[i].used) {
 			SunPool[i].frameIndex = (SunPool->frameIndex + 1) % 29;
 			if (SunPool[i].timer == 0) {
-				SunPool[i].y += 2;
+				SunPool[i].y += 4;
 			}
 			if (SunPool[i].y >= SunPool[i].goalY) {
 				SunPool[i].timer++;
@@ -276,13 +338,155 @@ void UpdateSunshine(){
 				}
 			}
 		}
+		else if (SunPool[i].xOff) {
+
+			//设置阳光偏移量
+			float goalX = 262;
+			float goalY = 0;
+			float angle = atan((SunPool[i].y - goalY) / (SunPool[i].x - goalX));
+			SunPool[i].xOff = 4 * cos(angle);
+			SunPool[i].yOff = 4 * sin(angle);
+
+			SunPool[i].x -= SunPool[i].xOff;
+			SunPool[i].y -= SunPool[i].yOff;
+			if (SunPool[i].x < 262 || SunPool[i].y < 0) {
+				SunPool[i].xOff = 0;
+				SunPool[i].yOff = 0;
+				SunValue += 25;
+			}
+		}
 	}
 }
 //创建僵尸
-void CreatZombie() {
-	int i;
+void CreateZombie() {
+	static int zmfre = 200;
+	static int count = 0;//调用次数
+	count++;
+	if (count >= zmfre) {
+		count = 0;
+		zmfre = rand() % 200 + 300;
+		int i;
+		int ZombieMax = sizeof(Zombies) / sizeof(Zombies[0]);
+		for (i = 0; i < ZombieMax && Zombies[i].used; i++);
+		if (i < ZombieMax) {
+			Zombies[i].used = true;
+			Zombies[i].x = W_width;
+			Zombies[i].row = rand() % 3;
+			Zombies[i].y = 172 + ( Zombies[i].row + 1) * 100;
+			Zombies[i].speed = 1;
+			Zombies[i].blood = 100;
+		}
+	}
+	
+
+}
+//更新僵尸
+void UpdateZombie() {
+
+	static int  count = 0;
+	count++;
+
+	if (count > 2) {
+		count = 0;
+		//更新僵尸的位置
+		int ZombieMax = sizeof(Zombies) / sizeof(Zombies[0]);
+		for (int i = 0; i < ZombieMax; i++) {
+			if (Zombies[i].used) {
+				Zombies[i].x -= Zombies[i].speed;
+				if (Zombies[i].x < 170) {
+					std::cout << "游戏结束！你被僵尸吃掉了脑子！\n";
+					MessageBox(NULL, "游戏结束！", "游戏结束！", 0);
+					exit(0);//待优化
+				}
+			}
+		} 
+
+		for (int i = 0; i < ZombieMax; i++) {
+			Zombies[i].frameIndex = (Zombies[i].frameIndex + 1) % 22;
+		}
+
+	}
+	
+}
+//发射子弹
+void ShootBullet() {
+	int rows[3] = { 0 };
+	int ZombieMax = sizeof(Zombies) / sizeof(Zombies[0]);
+	int BulletMax = sizeof(Bullets) / sizeof(Bullets[0]);
+	int DangerX = W_width - ImgZombie->getwidth();
+	for (int i = 0; i < ZombieMax; i++) {
+		if (Zombies[i].used && Zombies[i].x < DangerX) {
+			rows[Zombies[i].row] = 1;
+		}
+	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (map[i][j].type == WAN_DOU + 1 && rows[i]) {
+				static int count = 0;
+				count++;
+				if (count > 20) {
+					count = 0;
+					int k;
+					for (k = 0; k < BulletMax && Bullets[k].used; k++);
+					if (k < BulletMax) {
+						Bullets[k].used = true;
+						Bullets[k].row = i;
+						Bullets[k].speed = 6;
+						Bullets[k].blast = false;
+						Bullets[k].frameindex = 0;
+
+						int PlantX = 256 + j * 81;
+						int PlantY = 179 + i * 102 + 14;
+
+						Bullets[k].x = PlantX + ImgPlant[map[i][j].type - 1][0]->getwidth()-10;
+						Bullets[k].y = PlantY + 5;
+					}
+				}
+			}
+		}
+	}
+
+}
+//更新子弹
+void UpdateBullet() {
+	int BulletMax = sizeof(Bullets) / sizeof(Bullets[0]);
+	for (int i = 0; i < BulletMax; i++) {
+		if (Bullets[i].used) {
+			Bullets[i].x += Bullets[i].speed;	
+			if (Bullets[i].x > W_width) {
+				Bullets[i].used = false;
+			}
+		}
+		//子弹碰撞时
+		if (Bullets[i].blast) {
+			Bullets[i].frameindex++;
+			if (Bullets[i].frameindex > 3) {
+				Bullets[i].used = false;
+			}
+		}
+	}
+}
+//碰撞检测
+void CollisionCheck(){
+	int BulletMax = sizeof(Bullets) / sizeof(Bullets[0]);
 	int ZombieMax = sizeof(Zombies) / sizeof(Zombies[0]);
 
+	for (int i = 0; i < BulletMax; i++) {
+		if (Bullets[i].used == false || Bullets[i].blast) continue;
+
+		for (int j = 0; j < ZombieMax; j++) {
+			if (Zombies[j].used == false) continue;
+			int x1 = Zombies[j].x + 80;	
+			int x2 = Zombies[j].x + 110;	
+			int x = Bullets[i].x;
+			if (Bullets[i].row == Zombies[j].row && x > x1 && x< x2){
+				Zombies[j].blood -= 20;
+				Bullets[i].blast = true;
+				Bullets[i].speed = 0;
+			}
+		}
+	}
 }
 
 // 数据更新
@@ -303,7 +507,11 @@ void UpdateGame() {
 	}
 	CreateSunShine();//创建阳光
 	UpdateSunshine();//更新阳光
-	//CreatZombie();	 //创建僵尸
+	CreateZombie();	 //创建僵尸
+	UpdateZombie();	 //更新僵尸
+	ShootBullet();	 //发射子弹
+	UpdateBullet();	 //更新子弹
+	CollisionCheck();//豌豆子弹与僵尸的碰撞检测
 }
 //启动UI
 void StartUI(){
